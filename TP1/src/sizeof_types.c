d3ee53f00c0862f1f11361b3ca3d34879d0c3352
Johnboyke445 <123312562+Johnboyke445@users.noreply.github.com>
2025-11-21 11:08:55 +0100

    update char

---
+++ TP1/src/sizeof_types.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+ printf("La taille en octets :");
+
+printf("la taille en octets char en sizeof: " sizeof(signed char));
+printf("la taille en octets char en unsigned:" sizeof(unsigned char));
+}
