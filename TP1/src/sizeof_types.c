a25d592f9f152ef2d7b3c46c3cd5217933a3e937
Johnboyke445 <123312562+Johnboyke445@users.noreply.github.com>
2025-11-21 11:18:00 +0100

    update main
    Added size calculations for signed and unsigned short types.

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
 int main() {
- printf("La taille en octets :");
+ printf("La taille en octets :\n\n");
 
 printf("la taille en octets char en sizeof:  %zu octets\n", sizeof(signed char));
 printf("la taille en octets char en unsigned: %zu octets\n", sizeof(unsigned char));
+
+printf("la taille en octets short en sizeof:  %zu octets\n", sizeof(signed short));
+printf("la taille en octets short en unsigned: %zu octets\n", sizeof(unsigned short));
+
 }
+
+
 
 
