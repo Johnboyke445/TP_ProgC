4d1d8f2c0b4d31fcb115620cc8843a40ee6bb66f
Johnboyke445 <123312562+Johnboyke445@users.noreply.github.com>
2025-12-01 15:44:32 +0100

    Update calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,24 @@
+# include <stdio.h>
+
+int main(){
+  
+  int num1 = 12;
+  int num2 = 4;
+  char op ='+';
+
+  switch (op) {
+
+    case '+':
+       printf("%d + %d = %d\n", num1, num2, num1 + num2);
+            break;
+    case '-':
+     printf("%d - %d = %d\n", num1, num2, num1 - num2);
+            break;
+    case '*':
+      printf("%d * %d = %d\n", num1, num2, num1 * num2);
+            break;
+    
+    
+  }
+}
 
