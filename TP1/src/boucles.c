0671f862ca3359f73201476ec172b1efee14f97c
Johnboyke445 <123312562+Johnboyke445@users.noreply.github.com>
2025-12-01 14:54:14 +0100

    Update boucles.c

---
+++ TP1/src/boucles.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+
+int main(){
+  for (int i = 1; i<=compteur; i++){
+    for  (int j = 1; j <= i; j++){
+
+      if (i ==1 || i == 2 || i == compteur) {
+        printf("* ");
+      }
+      esle{
+        if (j == 1 || j == i)
+          printf("* ");
+      esle 
+        printf("# ")
+      }
+    }
+    printf("\n");
+  }
+return 0;
+}
